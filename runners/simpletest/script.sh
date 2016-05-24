2618dd6cddfa55f91cce6be66c7cad076bbf1f30
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-24 16:44:28 +0100

    another test

--- runners/simpletest/script.sh
+++ runners/simpletest/script.sh
@@ -18,6 +18,6 @@ fi
 
 cd "$DRUPAL_TI_DRUPAL_DIR"
 { php "$DRUPAL_TI_SIMPLETEST_FILE" --php $(which php) "${ARGS[@]}" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
-
+cat $DRUPAL_TI_SIMPLETEST_FILE
 egrep -i "([1-9]+ fail[s]?\s)|(Fatal error)|([1-9]+ exception[s]?\s)" /tmp/simpletest-result.txt && exit 1
 exit 0
