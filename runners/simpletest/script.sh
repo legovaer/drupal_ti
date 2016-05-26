2cd53c87d4ef3c4a776bec11d56fee1c937c623b
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-26 13:16:26 +0100

    another test

--- runners/simpletest/script.sh
+++ runners/simpletest/script.sh
@@ -2,16 +2,6 @@
 # @file
 # Simple script to run the tests via travis-ci.
 
-set -e $DRUPAL_TI_DEBUG
-
-export ARGS=( $DRUPAL_TI_SIMPLETEST_ARGS )
-
-if [ -n "$DRUPAL_TI_SIMPLETEST_GROUP" ]
-then
-        ARGS=( "${ARGS[@]}" "$DRUPAL_TI_SIMPLETEST_GROUP" )
-fi
-
-
 cd "$DRUPAL_TI_DRUPAL_DIR"
 phpcov execute $DRUPAL_TI_SCRIPT_DIR/utility/launch-simpletest.sh bash \
   --configuration $DRUPAL_TI_MODULES_PATH/scheduler/$DRUPAL_TI_PHPCOV_XML \
--- utility/launch-simpletest.sh
+++ utility/launch-simpletest.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e $DRUPAL_TI_DEBUG
+
+export ARGS=( $DRUPAL_TI_SIMPLETEST_ARGS )
+
+if [ -n "$DRUPAL_TI_SIMPLETEST_GROUP" ]
+then
+        ARGS=( "${ARGS[@]}" "$DRUPAL_TI_SIMPLETEST_GROUP" )
+fi
+
 { php "$DRUPAL_TI_SIMPLETEST_FILE" --php $(which php) "${ARGS[@]}" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
 cat $DRUPAL_TI_SIMPLETEST_FILE
 egrep -i "([1-9]+ fail[s]?\s)|(Fatal error)|([1-9]+ exception[s]?\s)" /tmp/simpletest-result.txt && exit 1
