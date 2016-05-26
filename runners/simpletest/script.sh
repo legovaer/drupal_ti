6b31266817e432649af0836db6ed530923a36cd2
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-26 13:44:33 +0100

    Displaying contents of the modules directory

--- runners/simpletest/script.sh
+++ runners/simpletest/script.sh
@@ -3,6 +3,8 @@
 # Simple script to run the tests via travis-ci.
 
 cd "$DRUPAL_TI_DRUPAL_DIR"
+
+ls -ls $DRUPAL_TI_MODULES_PATH
 phpcov execute $DRUPAL_TI_SCRIPT_DIR/utility/launch-simpletest.sh bash \
   --configuration $DRUPAL_TI_MODULES_PATH/scheduler/$DRUPAL_TI_PHPCOV_XML \
   --html $DRUPAL_TI_DRUPAL_DIR/coverage-report
